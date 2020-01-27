5490412376740a6285d2bb567a2e97366238b35f
Max Dmitriev <maxdmtrev@gmail.com>
2020-01-27 14:37:17 +0300

    Bath construct

--- frontend/src/components/construtor/dataBase2.js
+++ frontend/src/components/construtor/dataBase2.js
@@ -5,75 +5,51 @@ import image3 from "../../img/bath/kerama_floor/kerama_fregat_dark.jpg"
 import image4 from "../../img/bath/kerama_floor/kerama_prostone_dark.jpg"
 import image5 from "../../img/bath/kerama_floor/kerama_prostone_light.jpg"
 import image6 from "../../img/bath/kerama_floor/prostone_beige_light.jpg"
-import image14 from '../../img/room/door/dg-nyuta-white.jpg';
-import image15 from '../../img/room/door/dorren-white.jpg';
-import image16 from '../../img/room/door/glanta-grey.jpg';
-import image17 from '../../img/room/door/glanta-light-grey.jpg';
-import image18 from '../../img/room/door/glanta-white.jpg';
-import image19 from '../../img/room/door/profildoors_1vg.jpg';
-import image20 from '../../img/room/door/profildoors_1z.jpg';
-import image21 from '../../img/room/door/profildoors_u100_antracite.jpg';
-import image22 from '../../img/room/door/profildoors_u100_black.jpg';
-
+import image14 from '../../img/bath/kerama_wall/kerama_belkanto.jpg'
+import image15 from '../../img/bath/kerama_wall/kerama_belkanto_fregat_beige.jpg'
+import image16 from '../../img/bath/kerama_wall/kerama_kaleidoskop_white.jpg'
+import image17 from '../../img/bath/kerama_wall/kerama_kaleidoskop_white_beige.jpg'
+import image18 from '../../img/bath/kerama_wall/kerama_kaleidoskop_white_pink.jpg'
+import image19 from '../../img/bath/kerama_wall/kerama_prostone_light_grey_fregat.jpg'
 
 
 
 export default {
-  // srcDef: [
-  //   {
-  //     img: image, zIndex: 1, title: '', id: null, sampleImage: null, descript: ''
-  //   }, {
-  //     img: image, zIndex: 2, title: '', id: null, sampleImage: null, descript: ''
-  //   }, {
-  //     img: image, zIndex: 1, title: '', id: null, sampleImage: null, descript: ''
-  //   }, {
-  //     img: image, zIndex: 2, title: '', id: null, sampleImage: null, descript: ''
-  //   }, {
-  //     img: image, zIndex: 2, title: '', id: null, sampleImage: null, descript: ''
-  //   }
-  // ],
   srcSample:[
-    {
-      img: image1, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    }, {
-      img: image2, zIndex: 2, title: '22', id: null, sampleImage: null, descript: ''
-    }, {
-      img: image3, zIndex: 1, title: '', id: null, sampleImage: null, descript: ''
-    }, {
-      img: image4, zIndex: 2, title: '', id: null, sampleImage: null, descript: ''
-    }, {
-      img: image5, zIndex: 2, title: '', id: null, sampleImage: null, descript: ''
-    },{
-      img: image6, zIndex: 2, title: '', id: null, sampleImage: null, descript: ''
-    }
-  ],
-  srcDoor:[
-    {
-      img: image14, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },
-    {
-      img: image15, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },
-    {
-      img: image16, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },
-    {
-      img: image17, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },{
-      img: image18, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },
-    {
-      img: image19, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },
-    {
-      img: image20, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },
-    {
-      img: image21, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    },  {
-      img: image22, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
-    }
-  
-  
+    { img: image1, zIndex: 1, title: 'Бельканто', id: null, sampleImage: null, descript: 'Хорошая плитка' },
+    { img: image2, zIndex: 1, title: 'Фрегат', id: null, sampleImage: null, descript: 'ХЗ' },
+    { img: image3, zIndex: 1, title: '', id: null, sampleImage: null, descript: '' },
+    { img: image4, zIndex: 1, title: '', id: null, sampleImage: null, descript: '' },
+    { img: image5, zIndex: 1, title: '', id: null, sampleImage: null, descript: ''},
+    { img: image6, zIndex: 1, title: '', id: null, sampleImage: null, descript: '' }
+    ],
+  srcWall:[
+    { img: image14, zIndex: 1, title: '12', id: null, sampleImage: null, descript: '' },
+    { img: image15, zIndex: 1, title: '12', id: null, sampleImage: null, descript: '' },
+    { img: image16, zIndex: 1, title: '12', id: null, sampleImage: null, descript: '' },
+    { img: image17, zIndex: 1, title: '12', id: null, sampleImage: null, descript: '' },
+    { img: image18, zIndex: 1, title: '12', id: null, sampleImage: null, descript: '' },
+    { img: image19, zIndex: 1, title: '12', id: null, sampleImage: null, descript: '' }
+
+
+    // {
+    //   img: image16, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // },
+    // {
+    //   img: image17, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // },{
+    //   img: image18, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // },
+    // {
+    //   img: image19, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // },
+    // {
+    //   img: image20, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // },
+    // {
+    //   img: image21, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // },  {
+    //   img: image22, zIndex: 1, title: '12', id: null, sampleImage: null, descript: ''
+    // }
   ]
 }
