fcbf06d7949ad4da66b2bf1761adc6a8d41bcd5d
Irina Rysbaeva <irsbv@mail.ru>
2020-01-27 17:11:14 +0300

    database and database2 with description

--- frontend/src/components/construtor/dataBase2.js
+++ frontend/src/components/construtor/dataBase2.js
@@ -1,31 +1,28 @@
 import image from "../../img/bath/kerama_wall/kerama_belkanto.png";
-import image1 from "../../img/bath/kerama_floor/equipe_micro_revival.jpg";
-import image2 from "../../img/bath/kerama_floor/kerama_fregat_beige.jpg";
-import image3 from "../../img/bath/kerama_floor/kerama_fregat_dark.jpg";
-import image4 from "../../img/bath/kerama_floor/kerama_prostone_dark.jpg";
-import image5 from "../../img/bath/kerama_floor/kerama_prostone_light.jpg";
-import image6 from "../../img/bath/kerama_floor/prostone_beige_light.jpg";
-import image14 from "../../img/room/door/dg-nyuta-white.jpg";
-import image15 from "../../img/room/door/dorren-white.jpg";
-import image16 from "../../img/room/door/glanta-grey.jpg";
-import image17 from "../../img/room/door/glanta-light-grey.jpg";
-import image18 from "../../img/room/door/glanta-white.jpg";
-import image19 from "../../img/room/door/profildoors_1vg.jpg";
-import image20 from "../../img/room/door/profildoors_1z.jpg";
-import image21 from "../../img/room/door/profildoors_u100_antracite.jpg";
-import image22 from "../../img/room/door/profildoors_u100_black.jpg";
+import floor1 from "../../img/bath/kerama_floor/equipe_micro_revival.jpg";
+import floor2 from "../../img/bath/kerama_floor/kerama_fregat_beige.jpg";
+import floor3 from "../../img/bath/kerama_floor/kerama_fregat_dark.jpg";
+import floor4 from "../../img/bath/kerama_floor/kerama_prostone_dark.jpg";
+import floor5 from "../../img/bath/kerama_floor/kerama_prostone_light.jpg";
+import floor6 from "../../img/bath/kerama_floor/prostone_beige_light.jpg";
+import floor1p from "../../img/bath/kerama_floor/equipe_micro_revival.png";
+import floor2p from "../../img/bath/kerama_floor/kerama_fregat_beige.png";
+import floor3p from "../../img/bath/kerama_floor/kerama_fregat_dark.png";
+import floor4p from "../../img/bath/kerama_floor/kerama_prostone_dark.png";
+import floor5p from "../../img/bath/kerama_floor/kerama_prostone_light.png";
+import floor6p from "../../img/bath/kerama_floor/prostone_beige_light.png";
 import bath1 from "../../img/bath/bath/cersanit_octavia.jpg";
 import bath1p from "../../img/bath/bath/cersanit_octavia.png";
 import bath2 from "../../img/bath/bath/roca_contesa.jpg";
 import bath2p from "../../img/bath/bath/roca_contesa.png";
 import bath3 from "../../img/bath/bath/roca_continental.jpg";
 import bath3p from "../../img/bath/bath/roca_continental.png";
-import bath4 from "../../img/bath/bath/roca_continental.jpg";
-import bath4p from "../../img/bath/bath/roca_continental.png";
+import bath4 from "../../img/bath/bath/roca_princess.jpg";
+import bath4p from "../../img/bath/bath/roca_princess.png";
 import locker1 from "../../img/bath/locker/locker_white.jpg";
 import locker1p from "../../img/bath/locker/locker_white.png";
-import locker2 from "../../img/bath/locker/locker_white.jpg";
-import locker2p from "../../img/bath/locker/locker_white.png";
+import locker2 from "../../img/bath/locker/locker_wood.jpg";
+import locker2p from "../../img/bath/locker/locker_wood.png";
 import toilet1 from "../../img/bath/toilet/cersanit_parva.jpg";
 import toilet1p from "../../img/bath/toilet/cersanit_parva.png";
 import toilet2 from "../../img/bath/toilet/roca_gap.jpg";
@@ -40,52 +37,55 @@ import shower3 from "../../img/bath/shower/grohe_euphoria.jpg";
 import shower3p from "../../img/bath/shower/grohe_euphoria.png";
 import shower4 from "../../img/bath/shower/lemark_tropic.jpg";
 import shower4p from "../../img/bath/shower/lemark_tropic.png";
-import keramaWall1 from "../..//img/bath/kerama_wall/kerama_belkanto_fregat_beige.jpg";
-import keramaWall1p from "../..//img/bath/kerama_wall/kerama_belkanto_fregat_beige.png";
-import keramaWall2 from "../..//img/bath/kerama_wall/kerama_belkanto_fregat.jpg";
-import keramaWall2p from "../..//img/bath/kerama_wall/kerama_belkanto_fregat.png";
-import keramaWall3 from "../..//img/bath/kerama_wall/kerama_belkanto.jpg";
-import keramaWall3p from "../..//img/bath/kerama_wall/kerama_belkanto.png";
-import keramaWall4 from "../..//img/bath/kerama_wall/kerama_kaleidoskop_white_beige.jpg";
-import keramaWall4p from "../..//img/bath/kerama_wall/kerama_kaleidoskop_white_beige.png";
-import keramaWall5 from "../..//img/bath/kerama_wall/kerama_kaleidoskop_white_pink.jpg";
-import keramaWall5p from "../..//img/bath/kerama_wall/kerama_kaleidoskop_white_pink.png";
-import keramaWall6 from "../..//img/bath/kerama_wall/kerama_kaleidoskop_white.jpg";
-import keramaWall6p from "../..//img/bath/kerama_wall/kerama_kaleidoskop_white.png";
+import keramaWall1 from "../../img/bath/kerama_wall/kerama_belkanto_fregat_beige.jpg";
+import keramaWall1p from "../../img/bath/kerama_wall/kerama_belkanto_fregat_beige.png";
+import keramaWall2 from "../../img/bath/kerama_wall/kerama_belkanto_fregat.jpg";
+import keramaWall2p from "../../img/bath/kerama_wall/kerama_belkanto_fregat.png";
+import keramaWall3 from "../../img/bath/kerama_wall/kerama_belkanto.jpg";
+import keramaWall3p from "../../img/bath/kerama_wall/kerama_belkanto.png";
+import keramaWall4 from "../../img/bath/kerama_wall/kerama_kaleidoskop_white_beige.jpg";
+import keramaWall4p from "../../img/bath/kerama_wall/kerama_kaleidoskop_white_beige.png";
+import keramaWall5 from "../../img/bath/kerama_wall/kerama_kaleidoskop_white_pink.jpg";
+import keramaWall5p from "../../img/bath/kerama_wall/kerama_kaleidoskop_white_pink.png";
+import keramaWall6 from "../../img/bath/kerama_wall/kerama_kaleidoskop_white.jpg";
+import keramaWall6p from "../../img/bath/kerama_wall/kerama_kaleidoskop_white.png";
 
 export default {
   shower: [
     {
       img: shower1p,
       zIndex: 3,
-      title: "12",
+      title: "Bravat Opal, круг (Германия)",
       id: null,
       sampleImage: shower1,
-      descript: ""
+      descript:
+        "Душевая стойка и смесители. Корпус: латунь. Покрытие: Хром. Керамический картридж Sedal 35 mm. Аэратор Neoperl. Излив: 20 л/мин, Душ: 12 л/мин"
     },
     {
       img: shower2p,
       zIndex: 3,
-      title: "12",
+      title: "Bravat Opal, квадрат (Германия)",
       id: null,
       sampleImage: shower2,
-      descript: ""
+      descript:
+        "Душевая стойка и смесители. Корпус: латунь. Покрытие: Хром. Керамический картридж Sedal 35 mm. Аэратор Neoperl. Излив: 20 л/мин, Душ: 12 л/мин"
     },
     {
       img: shower3p,
       zIndex: 3,
-      title: "12",
+      title: "Grohe Euphoria (Германия)",
       id: null,
       sampleImage: shower3,
-      descript: ""
+      descript:
+        "Душевая стойка и смесители с термостатом (27475001). Покрытие: Хром. Душ 9.5 л/мин"
     },
     {
       img: shower4p,
       zIndex: 3,
-      title: "12",
+      title: "Lemark Tropic (Чехия)",
       id: null,
       sampleImage: shower4,
-      descript: ""
+      descript: "Душевая стойка и смесители. Покрытие: Хром. Съемная полка."
     }
   ],
 
@@ -93,50 +93,50 @@ export default {
     {
       img: keramaWall1p,
       zIndex: 3,
-      title: "12",
+      title: "Kerama Marazzi Бельканто/Фрегат беж",
       id: null,
       sampleImage: keramaWall1,
-      descript: ""
+      descript: "Керамическая плитка 15х40см"
     },
     {
       img: keramaWall2p,
       zIndex: 3,
-      title: "12",
+      title: "Kerama Marazzi Бельканто/Фрегат коричн.",
       id: null,
       sampleImage: keramaWall2,
-      descript: ""
+      descript: "Керамическая плитка 15х40см"
     },
     {
       img: keramaWall3p,
       zIndex: 3,
-      title: "12",
+      title: "Kerama Marazzi Бельканто",
       id: null,
       sampleImage: keramaWall3,
-      descript: ""
+      descript: "Керамическая плитка 15х40см"
     },
     {
       img: keramaWall4p,
       zIndex: 3,
-      title: "12",
+      title: "Kerama Marazzi Калейдоскоп белая/беж",
       id: null,
       sampleImage: keramaWall4,
-      descript: ""
+      descript: "Керамическая плитка 20х20см"
     },
     {
       img: keramaWall5p,
       zIndex: 3,
-      title: "12",
+      title: "Kerama Marazzi Калейдоскоп белая/розовая",
       id: null,
       sampleImage: keramaWall5,
-      descript: ""
+      descript: "Керамическая плитка 20х20см"
     },
     {
       img: keramaWall6p,
       zIndex: 3,
-      title: "12",
+      title: "Kerama Marazzi Калейдоскоп белая",
       id: null,
       sampleImage: keramaWall6,
-      descript: ""
+      descript: "Керамическая плитка 20х20см"
     }
   ],
 
@@ -144,26 +144,29 @@ export default {
     {
       img: toilet1p,
       zIndex: 3,
-      title: "12",
+      title: "Cersanit Parva (Польша)",
       id: null,
       sampleImage: toilet1,
-      descript: ""
+      descript:
+        "Напольный унитаз-компакт с микролифтом. Корпус: Фаянс. Система антивсплеск. Размеры: 36x61,5см"
     },
     {
       img: toilet2p,
       zIndex: 3,
-      title: "12",
+      title: "Roca The Gap (Испания)",
       id: null,
       sampleImage: toilet2,
-      descript: ""
+      descript:
+        "Подвесной унитаз с микролифтом. Корпус: Фаянс. Безободковый. Система антивсплеск. Размеры: 35x54см. Инсталляция Grohe Rapid"
     },
     {
       img: toilet3p,
       zIndex: 3,
-      title: "12",
+      title: "Roca Meridian (Испания)",
       id: null,
       sampleImage: toilet3,
-      descript: ""
+      descript:
+        "Подвесной унитаз с микролифтом. Корпус: Фаянс. Система антивсплеск. Размеры: 36x48см. Инсталляция Geberit Duofix."
     }
   ],
 
@@ -171,18 +174,20 @@ export default {
     {
       img: locker1p,
       zIndex: 3,
-      title: "12",
+      title: "IKEA ГОДМОРГОН / БРОВИКЕН",
       id: null,
       sampleImage: locker1,
-      descript: ""
+      descript:
+        "Белый шкаф для раковины с 2 ящиками. Ширина: 82см, Глубина: 49см, Высота: 68см. Одинарная раковина IKEA ОДЕНСВИК"
     },
     {
       img: locker2p,
       zIndex: 3,
-      title: "12",
+      title: "IKEA ГОДМОРГОН / БРОВИКЕН",
       id: null,
       sampleImage: locker2,
-      descript: ""
+      descript:
+        "Шкаф для раковины с 2 ящиками цвета белёный дуб. Ширина: 82см, Глубина: 49см, Высота: 68см. Одинарная раковина IKEA ОДЕНСВИК"
     }
   ],
   srcDef: [
@@ -227,127 +232,53 @@ export default {
       descript: ""
     }
   ],
-  srcSample: [
+  floor: [
     {
-      img: image1,
+      img: floor1p,
       zIndex: 1,
       title: "12",
       id: null,
-      sampleImage: null,
+      sampleImage: floor1,
       descript: ""
     },
     {
-      img: image2,
+      img: floor2p,
       zIndex: 2,
       title: "22",
       id: null,
-      sampleImage: null,
+      sampleImage: floor2,
       descript: ""
     },
     {
-      img: image3,
+      img: floor3p,
       zIndex: 1,
       title: "",
       id: null,
-      sampleImage: null,
+      sampleImage: floor3,
       descript: ""
     },
     {
-      img: image4,
+      img: floor4p,
       zIndex: 2,
       title: "",
       id: null,
-      sampleImage: null,
+      sampleImage: floor4,
       descript: ""
     },
     {
-      img: image5,
+      img: floor5p,
       zIndex: 2,
       title: "",
       id: null,
-      sampleImage: null,
+      sampleImage: floor5,
       descript: ""
     },
     {
-      img: image6,
+      img: floor6p,
       zIndex: 2,
       title: "",
       id: null,
-      sampleImage: null,
-      descript: ""
-    }
-  ],
-  srcDoor: [
-    {
-      img: image14,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image15,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image16,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image17,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image18,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image19,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image20,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image21,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
-      descript: ""
-    },
-    {
-      img: image22,
-      zIndex: 1,
-      title: "12",
-      id: null,
-      sampleImage: null,
+      sampleImage: floor6,
       descript: ""
     }
   ],
@@ -355,34 +286,38 @@ export default {
     {
       img: bath1p,
       zIndex: 3,
-      title: "12",
+      title: "Cersanit Octavia (Польша)",
       id: null,
       sampleImage: bath1,
-      descript: ""
+      descript:
+        "Акриловая ванна. Длина: 170см, Ширина: 70см, Глубина: 40см, Объём: 180л."
     },
     {
       img: bath2p,
       zIndex: 3,
-      title: "12",
+      title: "Roca Contesa (Испания)",
       id: null,
       sampleImage: bath2,
-      descript: ""
+      descript:
+        "Стальная ванна. Длина: 170см, Ширина: 70см, Глубина: 40см, Объём: 195л"
     },
     {
       img: bath3p,
       zIndex: 3,
-      title: "12",
+      title: "Roca Continental (Испания)",
       id: null,
       sampleImage: bath3,
-      descript: ""
+      descript:
+        "Чугунная ванна с противоскользящим покрытием. Длина: 170см, Ширина: 70см, Глубина: 42см, Объём: 182л"
     },
     {
       img: bath4p,
       zIndex: 3,
-      title: "12",
+      title: "Roca Princess (Испания)",
       id: null,
       sampleImage: bath4,
-      descript: ""
+      descript:
+        "Стальная ванна с ручками и противоскользящим покрытием. Длина: 170см, Ширина: 70см, Глубина: 43см, Объём: 190л"
     }
   ]
 };
